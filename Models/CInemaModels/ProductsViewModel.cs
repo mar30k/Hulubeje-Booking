a61d7a2a66aee1c91d599fbdf44e215b9726f566
Markos Beyene <markosbeyene1@gmail.com>
2023-12-22 17:24:11 +0300

    Movie Details page successfully added

---
+++ Models/CInemaModels/ProductsViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace HulubejeBooking.Models.CInemaModels
+{
+    public class ProductsViewModel
+    {
+        public List<Product>? Products { get; set; }
+        public string? MovieScheduleCode { get; set; }
+        public string? ArticleCode { get; set; }
+        public string? CompanyTinNumber { get; set; }
+        public string? CompanyName { get; set; }
+        public string? SpaceType { get; set; }
+        public string? MovieName { get; set; }
+        public string? ScheduleDate { get; set; }
+        public string? ScheduleTime { get; set; }
+        public string? HallName { get; set; }
+        public string? Dimension { get; set; }
+        public string? NumberOfSeats { get; set; }
+        public decimal Price { get; set; }
+        public Bill? Bill { get; set; }
+
+    }
+
+}
---
+++ Models/CInemaModels/SeatLayout.cs
@@ -0,0 +1,38 @@
+﻿namespace HulubejeBooking.Models.CInemaModels
+{
+    public class SeatLayout
+    {
+        public string? SpaceCode { get; set; }
+        public string? CompanyTinNumber { get; set; }
+        public string? CompanyName { get; set; }
+        public string? MovieName { get; set; }
+        public string? ArticleCode { get; set; }
+        public string? SelectedDate { get; set; }
+        public string? HallName { get; set; }
+        public string? UtcTime { get; set; }
+        public string? SpaceType { get; set; }
+        public string? Dimension { get; set; }
+        public string? MovieScheduleCode { get; set; }
+        public int MaxX { get; set; }
+        public int MaxY { get; set; }
+        public decimal Price { get; set; }
+        public List<SeatInfo>? Seats { get; set; }
+        public string? MaxSeats { get; set; }
+        public List<string>? BookedSeats { get; set; }
+        public List<string>? SoldSeats { get; set; }
+        public List<string>? AvailableSeats { get; set; }
+        public List<string>? TakenSeats { get; set; }
+
+    }
+
+    public class SeatInfo
+    {
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string? Type { get; set; }
+    }
+
+
+}
