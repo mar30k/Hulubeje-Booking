6791c9437b9200177b0182505fb9138a32726dbf
Markos Beyene <markosbeyene1@gmail.com>
2023-12-25 16:44:58 +0300

    Added two models Bus model for routes and company to use in the home page and Routes to deserialize the api response for getting all routes

---
+++ Models/BusModels/BusModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HulubejeBooking.Models.BusModels
+{
+    public class BusModel
+    {
+        public List<CompanyModel>? Company { get; set; }
+        public Routes? Route { get; set; }
+    }
+}
---
+++ Models/BusModels/Routes.cs
@@ -0,0 +1,25 @@
+﻿namespace HulubejeBooking.Models.BusModels
+{
+    public class Routes
+    {
+        public List<City>? Cities { get; set; }
+        public List<Route>? Route { get; set; }
+    }
+    public class City
+    {
+        public string? CityName { get; set; }
+        public int? CityCode { get; set; }
+    }
+
+    public class Route 
+    {
+        public int? FromCity { get; set; }
+        public List<ToCity>? ToCity { get; set; }
+    }
+
+    public class ToCity
+    {
+        public int? RouteId { get; set; }
+        public int? CityCode { get; set; }
+    }
+}
