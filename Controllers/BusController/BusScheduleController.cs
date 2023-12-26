7914bfab2b19455fd50ae6c2b693935b9533b9e2
Markos Beyene <markosbeyene1@gmail.com>
2023-12-26 11:47:19 +0300

    Added controller and view to display the bus schedules for the selected route

---
+++ Controllers/BusController/BusScheduleController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.Net.Http;
+using Newtonsoft.Json;
+using HulubejeBooking.Models.BusModels;
+
+namespace HulubejeBooking.Controllers.BusController
+{
+    public class BusScheduleController : Controller
+    {
+        private readonly HttpClient _httpClient;
+        public BusScheduleController()
+        {
+            _httpClient = new HttpClient()
+            {
+                BaseAddress = new Uri("http://192.168.1.25:8092/api/")
+            };
+
+            _httpClient.DefaultRequestHeaders.Add("x-api-key", "9BE090F9-7F52-4297-93A1-32D03D361DE9");
+        }
+        public async  Task<IActionResult> Index(string destination, DateTime travelDate)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync($"routeschedule/getschedulesbyroute?route={destination}&date={travelDate}");
+            if(response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                var scheduleData = JsonConvert.DeserializeObject<List<VwRouteSchedule>>(responseData);
+                return View(scheduleData);
+            }
+            return View(null);
+        }
+    }
+}
