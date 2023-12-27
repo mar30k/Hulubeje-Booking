32ca77a9a61dd3d94937e2e1b4893e1695b45e0c
Markos Beyene <markosbeyene1@gmail.com>
2023-12-27 16:10:33 +0300

    added a controller and view for vehicle seat layout

---
+++ Controllers/BusController/BusSeatLayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HulubejeBooking.Controllers.BusController
+{
+    public class BusSeatLayoutController : Controller
+    {
+        public IActionResult SeatLayout()
+        {
+            return View();
+        }
+    }
+}
