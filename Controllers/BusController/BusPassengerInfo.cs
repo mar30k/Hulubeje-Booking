335ce583525b6aa1fb23f62661156ec9ba8286f9
Markos Beyene <markosbeyene1@gmail.com>
2023-12-29 13:53:13 +0300

    displayed the schedule summary in the passenger info page

--- Controllers/BusController/BusPassengerInfo.cs
+++ Controllers/BusController/BusPassengerInfo.cs
@@ -4,8 +4,20 @@ namespace HulubejeBooking.Controllers.BusController
 {
     public class BusPassengerInfo : Controller
     {
-        public IActionResult PassengerInfo(int selectedButtonsCount, List<string> selectedButtons)
+        public IActionResult PassengerInfo(int selectedButtonsCount, List<string> selectedButtons, string plateNumber, string terminal, string distance, 
+            string tariff, string level, string route, string operatorName, string scheduleDate, string scheduleTime, string destinationCity, string depatureCity)
         {
+            ViewBag.PlateNumber = plateNumber;
+            ViewBag.Terminal = terminal;
+            ViewBag.Distance = distance;
+            ViewBag.Tariff = tariff;
+            ViewBag.Level = level;
+            ViewBag.Route = route;
+            ViewBag.OperatorName = operatorName;
+            ViewBag.Date = scheduleDate;
+            ViewBag.Time = scheduleTime;
+            ViewBag.DestinationCity = destinationCity;
+            ViewBag.DepatureCity = depatureCity;
             ViewBag.SelectedButtonsCount = selectedButtonsCount;
             ViewBag.SelectedButtons = selectedButtons;
             return View();
