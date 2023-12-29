4247a84e3db799685f0527d1bc2092f6ba416976
Markos Beyene <markosbeyene1@gmail.com>
2023-12-29 09:40:49 +0300

    Added contoller and view to collect the passenger information based on the number of seats selected

---
+++ Controllers/BusController/BusPassengerInfo.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HulubejeBooking.Controllers.BusController
+{
+    public class BusPassengerInfo : Controller
+    {
+        public IActionResult PassengerInfo(int selectedButtonsCount, List<string> selectedButtons)
+        {
+            ViewBag.SelectedButtonsCount = selectedButtonsCount;
+            ViewBag.SelectedButtons = selectedButtons;
+            return View();
+        }
+    }
+}
