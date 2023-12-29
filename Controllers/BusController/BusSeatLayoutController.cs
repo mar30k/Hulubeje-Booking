848f09478782f6da713bf7bdf7cbdb5cda525314
Markos Beyene <markosbeyene1@gmail.com>
2023-12-29 09:45:18 +0300

    reciverd the informations in the seatlayout controller and passed them to seatlayout page

--- Controllers/BusController/BusSeatLayoutController.cs
+++ Controllers/BusController/BusSeatLayoutController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HulubejeBooking.Models.BusModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HulubejeBooking.Controllers.BusController
 {
     public class BusSeatLayoutController : Controller
     {
-        public IActionResult SeatLayout()
+
+        public IActionResult SeatLayout(string plateNumber, string terminal, string distance, string tariff, string level, string route, string operatorName, string scheduleDate, string scheduleTime)
         {
-            return View();
+            var schedueleInfo = new SeatLayoutFormData
+            {
+                Distance = distance,
+                Level = level,
+                Route = route,
+                PlateNumber = plateNumber,
+                Terminal = terminal,
+                Tariff = tariff,
+                OperatorName = operatorName,
+                Date = scheduleDate,
+                Time = scheduleTime
+            };
+            return View(schedueleInfo);
         }
     }
 }
---
+++ Models/BusModels/SeatLayoutFormData.cs
@@ -0,0 +1,15 @@
+﻿namespace HulubejeBooking.Models.BusModels
+{
+    public class SeatLayoutFormData
+    {
+        public string? PlateNumber { get; set; }
+        public string? Terminal { get; set; }
+        public string? Distance { get; set; }
+        public string? Tariff { get; set; }
+        public string? Level { get; set; }
+        public string? Route { get; set; }
+        public string? OperatorName { get; set; }
+        public string? Time {  get; set; }  
+        public string? Date {  get; set; }  
+    }
+}
