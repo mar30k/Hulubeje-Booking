d5ca370571020719b6f9e508f8aa3d29abda0586
Markos Beyene <markosbeyene1@gmail.com>
2024-01-03 11:51:16 +0300

    In the confimation page displayed the passeger information that was filled in the passenger info page

---
+++ Controllers/BusController/BusConfirmationController.cs
@@ -0,0 +1,16 @@
+﻿using HulubejeBooking.Models.BusModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HulubejeBooking.Controllers.BusController
+{
+    public class BusConfirmationController : Controller
+    {
+        [HttpPost]
+        public IActionResult ConfirmationPage(List<PassengerModel> passengers, string tariff)
+        {
+            ViewBag.Tariff = tariff;
+            Console.WriteLine(tariff);
+            return View(passengers);
+        }
+    }
+}
---
+++ Models/BusModels/PassengerModel.cs
@@ -0,0 +1,13 @@
+﻿namespace HulubejeBooking.Models.BusModels
+{
+    public class PassengerModel
+    {
+        public string? PassengerName { get; set; }
+        public string? PassengerId { get; set; }
+        public int? PassengerAge { get; set; }
+        public string? Nationality { get; set; }
+        public string? Gender { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Email { get; set; }
+    }
+}
