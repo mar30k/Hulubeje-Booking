8f4e228dd98261b831ca846b46c52985fda028b6
Nathnael25 <nathnaels123@gmail.com>
2024-01-08 12:11:28 +0300

    payment option @ hulubeje website done

---
+++ Controllers/Payment/PaymentOptions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using HulubejeBooking.Models.PaymentModels;
+
+namespace Payment.Controllers
+{
+    public class PaymentOptions : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PaymentOption()
+        {
+            var paymentOptionsJson = HttpContext.Session.GetString("PaymentOptions");
+            if (!string.IsNullOrEmpty(paymentOptionsJson))
+            {
+                var viewPayments = JsonConvert.DeserializeObject<List<PaymentOptionModel>>(paymentOptionsJson);
+                var wrapper = new Wrapper
+                {
+                    PaymentOptions = viewPayments,  
+                    Boa = new BoAModel()  
+                };
+                return View(wrapper);
+            }
+            else
+            {
+                return View(new PaymentOptionModel());
+            }
+        }
+
+
+
+    }
+}
