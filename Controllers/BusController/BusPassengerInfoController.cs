e21e1cc3d215808a82fe3c720f3105a429e29996
Markos Beyene <markosbeyene1@gmail.com>
2024-01-25 15:19:10 +0300

    removed usage of viewBag from passengerInfo controller

--- Controllers/BusController/BusPassengerInfoController.cs
+++ Controllers/BusController/BusPassengerInfoController.cs
@@ -1,28 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HulubejeBooking.Models.BusModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HulubejeBooking.Controllers.BusController
 {
     public class BusPassengerInfoController : Controller
     {
-        public IActionResult PassengerInfo(int selectedButtonsCount, List<string> seatName, string plateNumber, string terminal, string distance, 
-            string tariff, string level, string route, string operatorName, string scheduleDate, string scheduleTime, string destinationCity, string depatureCity, string arrivialDate, string departureDate)
+        public IActionResult PassengerInfo(int selectedButtonsCount, List<string> seatName, string plateNumber, string terminal, int distance, List<string> seatId, int vehicleOperatorId, int routeScheduleId,
+            string tariff, string level, string route, string operatorName, string scheduleDate, DateTime scheduleTime, string destinationCity, string depatureCity, DateTime arrivialDate, DateTime departureDate)
         {
-            ViewBag.PlateNumber = plateNumber;
-            ViewBag.Terminal = terminal;
-            ViewBag.Distance = distance;
-            ViewBag.Tariff = tariff;
-            ViewBag.Level = level;
-            ViewBag.Route = route;
-            ViewBag.OperatorName = operatorName;
-            ViewBag.Date = scheduleDate;
-            ViewBag.Time = scheduleTime;
-            ViewBag.DestinationCity = destinationCity;
-            ViewBag.DepatureCity = depatureCity; 
-            ViewBag.SelectedButtonsCount = selectedButtonsCount;
-            ViewBag.SelectedButtons = seatName;
-            ViewBag.ArrivalDate = arrivialDate;
-            ViewBag.DepartureDate = departureDate;
-            return View();
+            var schedule = new VwRouteSchedule()
+            {
+                Id = routeScheduleId,
+                PlateNumber = plateNumber,
+                Terminal = terminal,
+                Distance = distance,
+                Amount = tariff,
+                LevelDesc = level,
+                OperatorName = operatorName,
+                RouteDescription = route,
+                Date = scheduleDate,
+                Time = scheduleTime,
+                DestCityName = destinationCity,
+                DepatureCity = depatureCity,
+                ArrivalDate = arrivialDate,
+                DepartureDate = departureDate,
+                SeatId = seatId,
+                VehicleOperatorId = vehicleOperatorId,
+                NoOfSeat = selectedButtonsCount,
+                SeatName = seatName,
+            };
+          return View(schedule);
         }
     }
 }
