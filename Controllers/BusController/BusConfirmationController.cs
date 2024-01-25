dbc9f18eb0315843e3071d9724d0eb058528c507
Markos Beyene <markosbeyene1@gmail.com>
2024-01-25 15:19:50 +0300

    mapped seatId for each passenger

--- Controllers/BusController/BusConfirmationController.cs
+++ Controllers/BusController/BusConfirmationController.cs
@@ -10,14 +10,21 @@ namespace HulubejeBooking.Controllers.BusController
     public class BusConfirmationController : Controller
     {
         [HttpPost]
-        public IActionResult ConfirmationPage(List<PassengerModel> passengers, string tariff, string depatureCity, string destinationCity, string terminal, string operatorName,
-            string date, string plateNumber, DateTime arrivalDate, DateTime departureDate)
+        public IActionResult ConfirmationPage(List<PassengerModel> passengers, List <string> seatId,string tariff, string depatureCity, string destinationCity, string terminal, string operatorName,
+            string date, string plateNumber, DateTime arrivalDate, DateTime departureDate, int vehicleOperatorId, int routeScheduleId)
         {
             string dateFormat = "ddd, MMM d, yyyy";
             DateTime dateTime = DateTime.ParseExact(date, dateFormat, CultureInfo.InvariantCulture);
             string formattedDate = dateTime.ToString("yyyy-MM-dd");
             string[] parts = tariff.Split(' ');
             decimal tarrifDecimal = decimal.Parse(parts[0]);
+            if (passengers.Count == seatId.Count)
+            {
+                for (int i = 0; i < passengers.Count; i++)
+                {
+                    passengers[i].SeatId = seatId[i];
+                }
+            }
             var schedule = new VwRouteSchedule()
             {
                 DestCityName = destinationCity,
@@ -29,6 +36,9 @@ namespace HulubejeBooking.Controllers.BusController
                 Terminal = terminal,
                 PlateNumber = plateNumber,
                 ArrivalDate = arrivalDate,
+                SeatId = seatId,
+                VehicleOperatorId = vehicleOperatorId,
+                Id = routeScheduleId,
             };
             var wrap = new Wrap()
             {
