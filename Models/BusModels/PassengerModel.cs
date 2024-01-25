b5917a9d78671fa79852841a7a09de821a85352f
Markos Beyene <markosbeyene1@gmail.com>
2024-01-25 15:21:05 +0300

    added seat id in PassengerModel

--- Models/BusModels/PassengerModel.cs
+++ Models/BusModels/PassengerModel.cs
@@ -10,5 +10,6 @@
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
         public string? DOB { get; set; }
+        public string? SeatId { get; set; }
     }
 }
