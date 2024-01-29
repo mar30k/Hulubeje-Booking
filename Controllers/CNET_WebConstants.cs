fe0a2db2270eedb949fc2d88c158210ece616a82
Nathnael25 <nathnaels123@gmail.com>
2024-01-29 17:27:35 +0300

    the login process has started

---
+++ Controllers/CNET_WebConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace HulubejeBooking.Controllers
+{
+    public class CNET_WebConstants
+    {
+        public static string ClaimsIssuer => "cnetERP";
+        public static string CookieScheme => "cnet.erp.v6";
+        public static string IdentificationCookie => "cnet.erp.v6.myId";
+
+        public const int IdentificationCookieLifeTime = 10080;
+        public const int IdentificationCookieDailyLifeTime = 1440;
+    }
+}
