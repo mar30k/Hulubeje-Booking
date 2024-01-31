c18c6fba22a8fdda6fc67390c7b9228cb69a3bc2
Markos Beyene <markosbeyene1@gmail.com>
2024-01-31 09:32:37 +0300

    changed gender to int and passed passenger name as first name middle name and last name

--- Models/PaymentModels/BusPaymentModel/PassengerInfo.cs
+++ Models/PaymentModels/BusPaymentModel/PassengerInfo.cs
@@ -29,7 +29,7 @@
         public string? FirstName { get; set; }
         public string? MiddleName { get; set; }
         public string? LastName { get; set; }
-        public string? Gender { get; set; }
+        public int? Gender { get; set; }
         public int? MaritalStatus { get; set; }
         public DateTime? Dob { get; set; }
         public string? PhoneNumber { get; set; }
