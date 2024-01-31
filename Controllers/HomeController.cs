095b524314c16801d03e0aaf41ec043297e41e75
Markos Beyene <markosbeyene1@gmail.com>
2024-01-31 09:38:25 +0300

    based on the values isVaild and isLoggedIn displayed logout button

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,22 +1,28 @@
 ﻿using HulubejeBooking.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
-
+using HulubejeBooking.Controllers.Authentication;
 namespace HulubejeBooking.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly AuthenticationManager _authenticationManager;
+        public HomeController(ILogger<HomeController> logger, AuthenticationManager authenticationManager)
         {
+            _authenticationManager = authenticationManager;
             _logger = logger;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()
         {
+            var identificationResult = await _authenticationManager.identificationValid();
+            ViewBag.isVaild = identificationResult.isValid;
+            ViewBag.isLoggedIn = identificationResult.isLoggedIn;
             return View();
         }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
