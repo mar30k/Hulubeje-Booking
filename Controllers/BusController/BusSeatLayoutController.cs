e249ab07b4f99984b672caf7ee9eab4e3414a7e4
Markos Beyene <markosbeyene1@gmail.com>
2024-01-31 09:27:59 +0300

    passed vehicle (Bus ) id to retrieve the seatlayout of it

--- Controllers/BusController/BusSeatLayoutController.cs
+++ Controllers/BusController/BusSeatLayoutController.cs
@@ -11,11 +11,12 @@ namespace HulubejeBooking.Controllers.BusController
             _httpClientFactory = httpClientFactory;
         }
         public async Task<IActionResult> SeatLayout(string plateNumber, string terminal, string distance, string tariff, string level, string route, string operatorName,string routeSchedule,
-            string scheduleDate, string scheduleTime, string destinationCity, string depatureCity, string arrivalDate, string departureDate, string vehicleOperatorId)
+            string scheduleDate, string scheduleTime, string destinationCity, string depatureCity, string arrivalDate, string departureDate, string vehicleOperatorId , int vehicle)
         {
             var busSeatLayoutClient = _httpClientFactory.CreateClient("BusBooking");
             var schedueleInfo = new BusSeatLayout
             {
+                
                 Distance = distance,
                 Level = level,
                 Route = route,
@@ -32,7 +33,7 @@ namespace HulubejeBooking.Controllers.BusController
                 VehicleOperatorId = vehicleOperatorId,
                 RouteScheduleId = routeSchedule,
             };
-            HttpResponseMessage response = await busSeatLayoutClient.GetAsync($"vehicles/getvehicleseatlayout?id={6915}");
+            HttpResponseMessage response = await busSeatLayoutClient.GetAsync($"vehicles/getvehicleseatlayout?id={vehicle}");
             if (response.IsSuccessStatusCode) { 
                string resopnseData = await response.Content.ReadAsStringAsync();
                var seatLayout = JsonConvert.DeserializeObject<SeatLayout>(resopnseData);
