bc0f05985a9a8b8201064a6ef4d6d146f599f10b
Markos Beyene <markosbeyene1@gmail.com>
2024-02-02 17:18:33 +0300

    made 2 api calls to get cinema and hotel history and bus history

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,51 @@
+﻿using HulubejeBooking.Models;
+using HulubejeBooking.Models.BusModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace HulubejeBooking.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public HistoryController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index(string phoneNumber)
+        {
+            return View();
+        }
+        public async Task<IActionResult> BusHistory(string phoneNumber)
+        {
+            var busClient = _httpClientFactory.CreateClient("BusBooking");
+            HttpResponseMessage response = await busClient.GetAsync($"history/gethistorybyphoneNumber?PhoneNumber={phoneNumber}");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                var history = JsonConvert.DeserializeObject<List<HistoryModel>>(responseData);
+                return View(history);
+            }
+            else
+            {
+                return View(null);
+            }
+        }
+        public async Task<IActionResult> CinemaHotelHistory(string phoneNumber)
+        {
+            var hulubejeClient = _httpClientFactory.CreateClient("CnetHulubeje");
+            HttpResponseMessage response = await hulubejeClient.GetAsync(hulubejeClient.BaseAddress + $"/order/GetOrdersByConsigneeCode?consigneeCode={phoneNumber}&page=1" );
+            if (response.IsSuccessStatusCode)
+            {
+                string responseData = await response.Content.ReadAsStringAsync();
+                var history = JsonConvert.DeserializeObject<OrdersModel>(responseData);
+                return View(history);
+            }else
+            {
+                return View(null);
+            }
+        }
+    }
+}
