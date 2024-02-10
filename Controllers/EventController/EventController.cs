89bdf54b0ea489803690ade6ca59ac97b1bd5107
Markos Beyene <markosbeyene1@gmail.com>
2024-02-10 10:38:52 +0300

    added acomming soon page for event

---
+++ Controllers/EventController/EventController.cs
@@ -0,0 +1,36 @@
+﻿using HulubejeBooking.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HulubejeBooking.Controllers.EventController
+{
+    public class EventController : Controller
+    {
+        private IHttpContextAccessor? _httpContextAccessor;
+
+        public EventController(IHttpContextAccessor? httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public IActionResult Index()
+        {
+            var userDataCookie = _httpContextAccessor?.HttpContext?.Request.Cookies[CNET_WebConstants.IdentificationCookie];
+            if (!string.IsNullOrEmpty(userDataCookie))
+            {
+                var user = JsonConvert.DeserializeObject<UserInformation>(userDataCookie);
+                ViewBag.UserName = user?.firstName;
+                ViewBag.LastName = user?.lastName;
+                ViewBag.MiddleName = user?.middleName;
+                ViewBag.Image = user?.personalattachment;
+                ViewBag.SuccessCode = user?.successCode;
+                ViewBag.Inumber = user?.idnumber;
+                ViewBag.Idtype = user?.idtype;
+                ViewBag.Dob = user?.dob;
+                ViewBag.Idattachment = user?.idattachment;
+                ViewBag.PhoneNumber = user?.phoneNumber;
+                ViewBag.EmailAddress = user?.emailAddress;
+            }
+            return View();
+        }
+    }
+}
