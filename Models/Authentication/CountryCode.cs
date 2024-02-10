f06cf4bc6e4d84f85c5edfc52b9c9d75aebc7fba
Markos Beyene <markosbeyene1@gmail.com>
2024-02-10 11:16:55 +0300

    added amodel to recieve country codes and names flags too

---
+++ Models/Authentication/CountryCode.cs
@@ -0,0 +1,24 @@
+﻿namespace HulubejeBooking.Models.Authentication
+{
+    public class CountryResponse
+    {
+        public Flags? Flags { get; set; } 
+        public Idd? Idd { get; set; }
+        public Names? Name { get; set; }
+    }
+    public class Idd
+    {
+        public string? Root { get; set; }
+        public List<string>? Suffixes { get; set; }
+    }
+    public class Names
+    {
+        public string? Common { get; set; }
+    }
+    public class Flags
+    {
+        public string? Png { get; set; } 
+        public string? Svg { get; set; } 
+        public string? Alt { get; set; } 
+    }
+}
