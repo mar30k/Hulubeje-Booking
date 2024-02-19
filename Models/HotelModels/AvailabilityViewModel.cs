319b4f02b0a1f537f32f4a1a7ffb544c63d78690
Markos Beyene <markosbeyene1@gmail.com>
2024-02-19 09:22:20 +0300

    made property AvailableRooms nullablle in AvailabilityViewModel

--- Models/HotelModels/AvailabilityViewModel.cs
+++ Models/HotelModels/AvailabilityViewModel.cs
@@ -2,7 +2,7 @@
 {
     public class AvailabilityViewModel
     {
-        public List<RoomModel> AvailableRooms { get; set; }
+        public List<RoomModel>? AvailableRooms { get; set; }
 		public List<string> AllPictureUrls
 		{
 			get => AvailableRooms?.SelectMany(room => room.PictureUrls).ToList() ?? new List<string>();
