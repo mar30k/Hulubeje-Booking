728009ea3cbf741a14cd530fa7e7b6ff7a69b7c3
Markos Beyene <markosbeyene1@gmail.com>
2024-02-19 09:23:07 +0300

    made properties Rooms Hotels nullable in HotelViewModel

--- Models/HotelModels/HotelViewModel.cs
+++ Models/HotelModels/HotelViewModel.cs
@@ -2,8 +2,8 @@
 {
     public class HotelViewModel
     {
-        public List<HotelModel> Hotels { get; set; }
-        public List<RoomModel> Rooms { get; set; }
+        public List<HotelModel>? Hotels { get; set; }
+        public List<RoomModel>? Rooms { get; set; }
         public List<Confirmation>? Confirm { get; internal set; }
     }
 }
