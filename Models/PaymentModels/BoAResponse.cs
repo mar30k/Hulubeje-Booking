2d36cf6e3e8e6ad3d45a6a9e4bd89269a8102aae
Markos Beyene <markosbeyene1@gmail.com>
2024-02-19 09:23:37 +0300

    made properties nullable in BoAResponse

--- Models/PaymentModels/BoAResponse.cs
+++ Models/PaymentModels/BoAResponse.cs
@@ -2,19 +2,19 @@
 {
     public class BoAResponse
     {
-        public string transactionReference { get; set; }
-        public bool isSuccessful { get; set; }
-        public List<string> errorMessages { get; set; }
-        public BoAAdditionalParametersModel additionalParameters { get; set; }
+        public string? transactionReference { get; set; }
+        public bool? isSuccessful { get; set; }
+        public List<string>? errorMessages { get; set; }
+        public BoAAdditionalParametersModel? additionalParameters { get; set; }
     }
 
     public class BoAAdditionalParametersModel
     {
-        public string RedirectUrl { get; set; }
+        public string? RedirectUrl { get; set; }
     }
     public class PaymentResponse
     {
-        public string cardText { get; set; }
-        public string RedirectUrl { get; set; }
+        public string? cardText { get; set; }
+        public string? RedirectUrl { get; set; }
     }
 }
