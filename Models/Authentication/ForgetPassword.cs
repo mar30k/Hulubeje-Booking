c043578a7427814a59a3158ec6724c38a10f08e6
Markos Beyene <markosbeyene1@gmail.com>
2024-02-20 10:35:06 +0300

    model for forget password

---
+++ Models/Authentication/ForgetPassword.cs
@@ -0,0 +1,20 @@
+﻿namespace HulubejeBooking.Models.Authentication
+{
+    public class ForgetPassword
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public int Type { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Nationality { get; set; }
+        public DateTime DOB { get; set; }
+        public string Title { get; set; }
+        public string Gender { get; set; }
+        public string Religion { get; set; }
+        public string Preference { get; set; }
+        public bool IsActive { get; set; }
+        public string Remark { get; set; }
+    }
+}
