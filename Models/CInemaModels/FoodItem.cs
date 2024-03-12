7c11af66093e4410929026553bdabb8eaac94588
Markos Beyene <markosbeyene1@gmail.com>
2024-03-12 16:54:41 +0300

    a model to fetch food and beverage response

---
+++ Models/CInemaModels/FoodItem.cs
@@ -0,0 +1,33 @@
+﻿namespace HulubejeBooking.Models.CInemaModels
+{
+    public class FoodItem
+    {
+        public bool? IsSuccessful { get; set; }
+        public List<FoodCategory>? Data { get; set; }
+        public string[]? ErrorMessages { get; set; }
+        public object? AdditionalParameters { get; set; }
+    }
+
+    public class FoodCategory
+    {
+        public int? Code { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public List<FoodCategory>? Children { get; set; }
+        public decimal? Price { get; set; }
+        public string? Currency { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Pictures { get; set; }
+        public string? Color { get; set; }
+        public string? Size { get; set; }
+        public string? Brand { get; set; }
+        public string? Weight { get; set; }
+        public string? Model { get; set; }
+        public string? Manufacturer { get; set; }
+        public string? CountryOrigin { get; set; }
+        public decimal? PreviousValue { get; set; }
+        public string? PackageForm { get; set; }
+    }
+
+
+}
--- Models/CInemaModels/ProductsViewModel.cs
+++ Models/CInemaModels/ProductsViewModel.cs
@@ -3,6 +3,7 @@
     public class ProductsViewModel
     {
         public List<Product>? Products { get; set; }
+        public FoodItem? FoodItem { get; set; }
         public string? MovieScheduleCode { get; set; }
         public string? ArticleCode { get; set; }
         public string? CompanyTinNumber { get; set; }
@@ -13,6 +14,7 @@
         public string? ScheduleTime { get; set; }
         public string? HallName { get; set; }
         public string? Dimension { get; set; }
+        public string? SeatCacheKey { get; set; }
         public string? NumberOfSeats { get; set; }
         public string? BranchCode { get; set; }
         public decimal Price { get; set; }
