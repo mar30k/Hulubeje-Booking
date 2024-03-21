a638cd58921337bdaad2d5165b50b137ac6a453c
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:53:47 +0300

    added a model to accept TransactionId api response

---
+++ Models/PaymentModels/TransactionId.cs
@@ -0,0 +1,11 @@
+﻿namespace HulubejeBooking.Models.PaymentModels
+{
+    public class TransactionId
+    {
+        public bool IsSuccessful { get; set; }
+        public string? Data { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+        public List<string>? AdditionalParameters { get; set; }
+    }
+
+}
