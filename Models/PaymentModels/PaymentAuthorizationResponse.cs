4100162fda48340c20bd130fc202ad58b4ce6bb9
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:48:54 +0300

    added a model to accept PaymentAuthorizationResponse api response

---
+++ Models/PaymentModels/PaymentAuthorizationResponse.cs
@@ -0,0 +1,19 @@
+﻿namespace HulubejeBooking.Models.PaymentModels
+{
+    public class PaymentAuthorizationResponse
+    {
+        public bool IsSuccessful { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+        public AdditionalParameter? AdditionalParameters { get; set; }
+        public string? TransactionReference { get; set; }
+    }
+
+    public class AdditionalParameter
+    {
+        public string? RedirectUrl { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Type { get; set; }
+        public bool? IsAsyncMode { get; set; }
+    }
+
+}
