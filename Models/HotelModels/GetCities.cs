a51ddc5f95dc1181fd90e5ddfa4af8384888bb8e
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:39:48 +0300

    added a new model to accept response from getcities endpoint

---
+++ Models/HotelModels/GetCities.cs
@@ -0,0 +1,23 @@
+﻿namespace HulubejeBooking.Models.HotelModels
+{
+    public class CityDatas
+    {
+        public int? Id { get; set; }
+        public int? Country { get; set; }
+        public string? CountryName { get; set; }
+        public string? CityName { get; set; }
+        public string? CityImage { get; set; }
+        public string? AlternativeName { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public int? Population { get; set; }
+    }
+
+    public class GetCities
+    {
+        public bool IsSuccessful { get; set; }
+        public List<CityDatas>? Data { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+        public List<string>? AdditionalParameters { get; set; }
+    }
+}
