7b66aa45b2f55dbf1cff107aa1b1b0b777a8f18f
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:52:44 +0300

    a model added to accept save api request response

---
+++ Models/PaymentModels/SaveResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace HulubejeBooking.Models.PaymentModels
+{
+    public class SaveResponse
+    {
+        public bool? IsSuccessful { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+        public List<string>? AdditionalParameters { get; set; }
+        public string? TransactionReference { get; set; }
+    }
+}
