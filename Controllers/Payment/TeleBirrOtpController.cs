f70fdfd196971f11b97f183c42a8e5bb34d40e8c
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:32:55 +0300

    added a new telebirr otp controller to handel payment options with otp type

---
+++ Controllers/Payment/TeleBirrOtpController.cs
@@ -0,0 +1,57 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+
+namespace HulubejeBooking.Controllers.Payment
+{
+    public class TeleBirrOtpController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private IHttpContextAccessor? _httpContextAccessor;
+        private readonly AuthenticationManager _authenticationManager;
+        public TeleBirrOtpController(IHttpClientFactory httpClientFactory, IHttpContextAccessor? httpContextAccessor, AuthenticationManager authenticationManager)
+        {
+            _httpClientFactory = httpClientFactory;
+            _httpContextAccessor = httpContextAccessor;
+            _authenticationManager = authenticationManager;
+        }
+        public async Task<IActionResult> IndexAsync()
+        {
+            var paymentOptionsJson = HttpContext.Session.GetString("PaymentOptions");
+            var value = HttpContext.Session.GetString("cinema");
+            if (value != null)
+            {
+                ViewBag.CoutDown = value;
+            }
+            var errorValue = HttpContext.Session.GetString("error");
+            if (errorValue != null)
+            {
+                TempData["ErrorMessage"] = "Incorrect Otp Or Couldn't Process Your Payment!";
+            }
+            HttpContext.Session.Remove("error");
+            var userDataCookie = _httpContextAccessor?.HttpContext?.Request.Cookies[CNET_WebConstants.IdentificationCookie];
+            if (!string.IsNullOrEmpty(userDataCookie))
+            {
+                var user = JsonConvert.DeserializeObject<UserInformation>(userDataCookie);
+                ViewBag.FirstName = user?.firstName;
+                ViewBag.LastName = user?.lastName;
+                ViewBag.MiddleName = user?.middleName;
+                ViewBag.Personalattachment = user?.personalattachment;
+                ViewBag.SuccessCode = user?.successCode;
+                ViewBag.Idnumber = user?.idnumber;
+                ViewBag.Idtype = user?.idtype;
+                ViewBag.Dob = user?.dob;
+                ViewBag.Idattachment = user?.idattachment;
+                ViewBag.PhoneNumber = user?.phoneNumber;
+                ViewBag.EmailAddress = user?.emailAddress;
+            }
+            var b = await _authenticationManager.identificationValid();
+            ViewBag.isVaild = b.isValid;
+            ViewBag.isLoggedIn = b.isLoggedIn;
+            return View();
+        }
+    }
+}
