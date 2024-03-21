83f8e6b5968ce6974c315e6ad0e513be16f50c5f
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:33:27 +0300

    a new controller to handel payment options that are type ussd push in v7

---
+++ Controllers/Payment/UssdPushController.cs
@@ -0,0 +1,43 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HulubejeBooking.Controllers.Payment
+{
+    public class UssdPushController : Controller
+    {
+        private AuthenticationManager _authenticationManager;
+        private IHttpContextAccessor? _httpContextAccessor;
+        public UssdPushController(IHttpContextAccessor? httpContextAccessor , AuthenticationManager authenticationManager)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _authenticationManager = authenticationManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var userDataCookie = _httpContextAccessor?.HttpContext?.Request.Cookies[CNET_WebConstants.IdentificationCookie];
+            string? code = null;
+            if (!string.IsNullOrEmpty(userDataCookie))
+            {
+                var user = JsonConvert.DeserializeObject<UserInformation>(userDataCookie);
+                code = user?.phoneNumber;
+                ViewBag.FirstName = user?.firstName;
+                ViewBag.LastName = user?.lastName;
+                ViewBag.MiddleName = user?.middleName;
+                ViewBag.Personalattachment = user?.personalattachment;
+                ViewBag.SuccessCode = user?.successCode;
+                ViewBag.Idnumber = user?.idnumber;
+                ViewBag.Idtype = user?.idtype;
+                ViewBag.Dob = user?.dob;
+                ViewBag.Idattachment = user?.idattachment;
+                ViewBag.PhoneNumber = user?.phoneNumber;
+                ViewBag.EmailAddress = user?.emailAddress;
+            }
+            var b = await _authenticationManager.identificationValid();
+            ViewBag.isVaild = b.isValid;
+            ViewBag.isLoggedIn = b.isLoggedIn;
+            return View();
+        }
+    }
+}
