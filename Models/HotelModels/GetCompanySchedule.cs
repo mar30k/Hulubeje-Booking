4c4bd2e845654d3f3e4097af8ed83210e7616018
Markos Beyene <markosbeyene1@gmail.com>
2024-03-21 15:45:03 +0300

    added a model to accept GetCompanySchedule api response

---
+++ Models/HotelModels/GetCompanySchedule.cs
@@ -0,0 +1,26 @@
+﻿namespace HulubejeBooking.Models.HotelModels
+{
+    public class Amenities
+    {
+        public int? Index { get; set; }
+        public int? Code { get; set; }
+        public string? Name { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Description { get; set; }
+    }
+
+    public class CompanySchedule
+    {
+        public string? Status { get; set; }
+        public List<object>? Schedules { get; set; }
+        public List<Amenity>? Aminities { get; set; }
+    }
+
+    public class GetCompanySchedule
+    {
+        public bool? IsSuccessful { get; set; }
+        public CompanySchedule? Data { get; set; }
+        public List<string>? ErrorMessages { get; set; }
+        public List<string>? AdditionalParameters { get; set; }
+    }
+}
