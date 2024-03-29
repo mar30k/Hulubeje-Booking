cb2b9b1b528dc643e24d355a35fd6e59a6859140
Markos Beyene <markosbeyene1@gmail.com>
2024-03-29 11:45:16 +0300

    added buffers implementation of IBuffer interface

---
+++ Controllers/Buffers.cs
@@ -0,0 +1,26 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.Authentication;
+using System;
+
+namespace HulubejeBooking.Controllers
+{
+    public class Buffers : IBuffer
+    {
+        private UserData? _customer;
+
+        public virtual void AddCustomerToBuffer(UserData customer)
+        {
+            _customer = customer;
+        }
+
+        public virtual void DeleteCurrentCustomerFromBuffer()
+        {
+            _customer = null;
+        }
+
+        public virtual async Task<UserData> GetCurrentCustomerFromBuffer()
+        {
+            return _customer;
+        }
+    }
+}
