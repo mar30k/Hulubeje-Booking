f0668dd7962ee87476e053fa4df2bbc9658770aa
Markos Beyene <markosbeyene1@gmail.com>
2024-03-29 11:44:48 +0300

    added Ibuffer interface

---
+++ Controllers/IBuffer.cs
@@ -0,0 +1,13 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.Authentication;
+using System.Security.Cryptography;
+
+namespace HulubejeBooking.Controllers
+{
+    public interface IBuffer
+    {
+        void AddCustomerToBuffer(UserData customer);
+        void DeleteCurrentCustomerFromBuffer();
+        Task<UserData> GetCurrentCustomerFromBuffer();
+    }
+}
