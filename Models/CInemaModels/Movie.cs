40d4f89e6f194ca1f483794abe47adf481352870
Markos Beyene <markosbeyene1@gmail.com>
2024-05-17 16:28:07 +0300

    modified movie and company data model's to accomodate trending movies

--- Models/CInemaModels/Movie.cs
+++ Models/CInemaModels/Movie.cs
@@ -1,4 +1,5 @@
 ﻿using HulubejeBooking.Models.Authentication;
+using HulubejeBooking.Models.BusModels;
 
 namespace HulubejeBooking.Models.CInemaModels
 {
@@ -6,6 +7,7 @@ namespace HulubejeBooking.Models.CInemaModels
     {
         public bool? IsSuccessful { get; set; }
         public List<CompanyData>? Data { get; set; }
+        public List<CompanyData>? TrendingMovies { get; set; }
         public string[]? ErrorMessages { get; set; }
         public string[]? AdditionalParameters { get; set; }
         public cookieValidation? CookieValidation { get; set; }
@@ -20,6 +22,30 @@ namespace HulubejeBooking.Models.CInemaModels
         public int? BranchCode { get; set; }
         public string? BranchName { get; set; }
         public List<Movies>? Movies { get; set; }
+        public bool IsTrendingDown { get; set; }
+        public bool IsTrendingUp { get; set; }
+        public Stat? Stat { get; set; }
+        public double Rating { get; set; }
+        public int RatingCount { get; set; }
+    }
+    public class Stat
+    {
+        public Count? Count { get; set; }
+        public Place? Place { get; set; }
+    }
+
+    public class Count
+    {
+        public int Day { get; set; }
+        public int Week { get; set; }
+        public int Month { get; set; }
+    }
+
+    public class Place
+    {
+        public string Day { get; set; }
+        public string Week { get; set; }
+        public string Month { get; set; }
     }
 
     public class Movies
@@ -36,6 +62,8 @@ namespace HulubejeBooking.Models.CInemaModels
         public string? MovieLanguageDescription { get; set; }
         public string? SubtitleLanguage { get; set; }
         public string? MovieCategoryName { get; set; }
+        public decimal Rating { get; set; }
+        public int RatingCount { get; set; }
         public string? RatingName { get; set; }
         public string? Duration { get; set; }
         public string? Director { get; set; }
@@ -59,7 +87,7 @@ namespace HulubejeBooking.Models.CInemaModels
 
     public class MovieSpace
     {
-        public int?  Id { get; set; }
+        public int? Id { get; set; }
         public int? SpaceId { get; set; }
         public string? CinemaHall { get; set; }
         public decimal? PriceValue { get; set; }
