fa9c909becbdd65c4aabd1c78b4902781e9f3506
Markos Beyene <markosbeyene1@gmail.com>
2024-06-28 15:06:11 +0300

    added new event schedule controller and view

---
+++ Controllers/EventController/EventScheduleController.cs
@@ -0,0 +1,54 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.EventModels;
+using HulubejeBooking.Models.HotelModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+
+namespace HulubejeBooking.Controllers.EventController
+{
+	public class EventScheduleController : Controller
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+		private IHttpContextAccessor? _httpContextAccessor;
+		private AuthenticationManager _authenticationManager;
+		public EventScheduleController(IHttpContextAccessor? httpContextAccessor, AuthenticationManager authenticationManager, IHttpClientFactory httpClientFactory)
+		{
+			_httpContextAccessor = httpContextAccessor;
+			_authenticationManager = authenticationManager;
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IActionResult> IndexAsync(int branchCode, int companyCode)
+		{
+			var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
+			var token = "";
+			var identificationResult = await _authenticationManager.identificationValid();
+			if (identificationResult != null)
+			{
+				token = identificationResult.UserData.Token;
+				ViewBag.isVaild = identificationResult.isValid;
+				ViewBag.isLoggedIn = identificationResult.isLoggedIn;
+				ViewBag.FirstName = identificationResult?.UserData.FirstName;
+				ViewBag.LastName = identificationResult?.UserData.LastName;
+				ViewBag.MiddleName = identificationResult?.UserData.MiddleName;
+				ViewBag.Personalattachment = identificationResult?.UserData.PersonalAttachment;
+				ViewBag.Idnumber = identificationResult?.UserData.IdNumber;
+				ViewBag.Idtype = identificationResult?.UserData.IdType;
+				ViewBag.Dob = identificationResult?.UserData.Dob;
+				ViewBag.Idattachment = identificationResult?.UserData.IdAttachment;
+				ViewBag.PhoneNumber = identificationResult?.UserData.Code;
+				ViewBag.EmailAddress = identificationResult?.UserData.Email;
+			}
+			var eventResponse = new EventResponse();
+            _v7Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage getEvent = await _v7Client.GetAsync($"event/get?company={companyCode}&branch={branchCode}");
+            if (getEvent.IsSuccessStatusCode)
+            {
+                string getEventData = await getEvent.Content.ReadAsStringAsync();
+				eventResponse = JsonConvert.DeserializeObject<EventResponse>(getEventData);
+            }
+            return View(eventResponse);
+		}
+	}
+}
