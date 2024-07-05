3445243912064bfcda0cc85e3156e8b476604f38
Markos Beyene <markosbeyene1@gmail.com>
2024-07-05 16:54:09 +0300

    added IQRCodeGenerator service to generate QR codes

---
+++ Controllers/IQRCodeGeneratorService.cs
@@ -0,0 +1,25 @@
+﻿using QRCoder;
+
+namespace HulubejeBooking.Controllers
+{
+    public interface IQRCodeGeneratorService
+    {
+        byte[] GenerateQRCode(string text);
+    }
+
+    public class QRCodeGeneratorService : IQRCodeGeneratorService
+    {
+        public byte[] GenerateQRCode(string text)
+        {
+            byte[] qrCode = new byte[0];
+            if (!string.IsNullOrEmpty(text))
+            {
+                QRCodeGenerator qrCodeGenerator = new QRCodeGenerator();
+                QRCodeData data = qrCodeGenerator.CreateQrCode(text, QRCodeGenerator.ECCLevel.Q);
+                BitmapByteQRCode bitmap = new BitmapByteQRCode(data);
+                qrCode = bitmap.GetGraphic(20);
+            }
+            return qrCode;
+        }
+    }
+}
