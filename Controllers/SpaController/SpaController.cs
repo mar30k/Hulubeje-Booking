71220fc6ec5693bab5c98cb36888ed79222675ae
Markos Beyene <markosbeyene1@gmail.com>
2024-08-03 11:18:44 +0300

    made api calls for getcompaniesbytype in spa controller

--- Controllers/SpaController/SpaController.cs
+++ Controllers/SpaController/SpaController.cs
@@ -1,7 +1,10 @@
 ﻿using HulubejeBooking.Controllers.Authentication;
 using HulubejeBooking.Models.Authentication;
+using HulubejeBooking.Models.HotelModels;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using System.Net.Http;
 
 namespace HulubejeBooking.Controllers.SpaController
 {
@@ -9,17 +12,22 @@ namespace HulubejeBooking.Controllers.SpaController
     {
         private IHttpContextAccessor? _httpContextAccessor;
         private AuthenticationManager _authenticationManager;
+        private IHttpClientFactory _httpClientFactory;
 
-        public SpaController(IHttpContextAccessor? httpContextAccessor, AuthenticationManager? authenticationManager)
+        public SpaController(IHttpContextAccessor? httpContextAccessor, AuthenticationManager? authenticationManager, IHttpClientFactory httpClientFactory)
         {
             _httpContextAccessor = httpContextAccessor;
             _authenticationManager = authenticationManager;
+            _httpClientFactory = httpClientFactory;
         }
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index()
         {
+            var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
+            var token = "";
             var identificationResult = await _authenticationManager.identificationValid();
             if (identificationResult != null)
             {
+                token = identificationResult.UserData.Token;
                 ViewBag.isVaild = identificationResult.isValid;
                 ViewBag.isLoggedIn = identificationResult.isLoggedIn;
                 ViewBag.FirstName = identificationResult?.UserData.FirstName;
@@ -33,7 +41,15 @@ namespace HulubejeBooking.Controllers.SpaController
                 ViewBag.PhoneNumber = identificationResult?.UserData.Code;
                 ViewBag.EmailAddress = identificationResult?.UserData.Email;
             }
-            return View();
+            var getcompaniesbytype = new GetcompaniesbyType();
+            _v7Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage getcompaniesbytypeResponse = await _v7Client.GetAsync("routing/getcompaniesbytype?industryType=1990");
+            if (getcompaniesbytypeResponse.IsSuccessStatusCode)
+            {
+                string getcompaniesbytypeData = await getcompaniesbytypeResponse.Content.ReadAsStringAsync();
+                getcompaniesbytype = getcompaniesbytypeData != null ? JsonConvert.DeserializeObject<GetcompaniesbyType>(getcompaniesbytypeData) : new GetcompaniesbyType();
+            }
+            return View(getcompaniesbytype);
         }
     }
 }
