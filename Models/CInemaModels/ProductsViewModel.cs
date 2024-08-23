c51680185ac945c7788ba34f5597a1f05b95e157
Markos Beyene <markosbeyene1@gmail.com>
2024-08-23 15:09:31 +0300

    changed data types some properties in productsviewmodel

--- Models/CInemaModels/ProductsViewModel.cs
+++ Models/CInemaModels/ProductsViewModel.cs
@@ -4,20 +4,21 @@
     {
         public List<Product>? Products { get; set; }
         public FoodItem? FoodItem { get; set; }
-        public string? MovieScheduleCode { get; set; }
-        public string? ArticleCode { get; set; }
+        public int? MovieScheduleCode { get; set; }
+        public int? ArticleCode { get; set; }
         public string? CompanyTinNumber { get; set; }
         public string? CompanyName { get; set; }
-        public string? CompanyCode { get; set; }
+        public int? CompanyCode { get; set; }
         public string? SpaceType { get; set; }
         public string? MovieName { get; set; }
-        public string? ScheduleDate { get; set; }
+        public DateTime? ScheduleDate { get; set; }
         public string? ScheduleTime { get; set; }
         public string? HallName { get; set; }
         public string? Dimension { get; set; }
         public string? SeatCacheKey { get; set; }
-        public string? NumberOfSeats { get; set; }
-        public string? BranchCode { get; set; }
+        public string? Seats { get; set; }
+        public int? BranchCode { get; set; }
+        public int? SpaceID { get; set; }
         public string? BranchName { get; set; }
         public decimal Price { get; set; }
         public Bill? Bill { get; set; }
