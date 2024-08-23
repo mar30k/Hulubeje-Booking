c6ecce66ffcd0c22de137bcdce40ae16958e67e2
Markos Beyene <markosbeyene1@gmail.com>
2024-08-23 15:08:42 +0300

    refactored code in home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -28,74 +28,41 @@ namespace HulubejeBooking.Controllers
 		public async Task<IActionResult> Index()
         {
             var identificationResult = await _authenticationManager.identificationValid();
-            while (identificationResult==null || identificationResult?.UserData?.FirstName == null)
-            {
-                identificationResult = await _authenticationManager.identificationValid();
-            }
+            //while (identificationResult==null || identificationResult?.UserData?.FirstName == null)
+            //{
+            //    identificationResult = await _authenticationManager.identificationValid();
+            //}
             var Picures = new Images
             {
                 CookieValidation = identificationResult
             };
             if (identificationResult != null)
             {
-                ViewBag.FirstName = identificationResult?.UserData.FirstName;
-                ViewBag.LastName = identificationResult?.UserData.LastName;
-                ViewBag.MiddleName = identificationResult?.UserData.MiddleName;
-                ViewBag.Personalattachment = identificationResult?.UserData.PersonalAttachment;
-                ViewBag.Idnumber = identificationResult?.UserData.IdNumber;
-                ViewBag.Idtype = identificationResult?.UserData.IdType;
-                ViewBag.Dob = identificationResult?.UserData.Dob;
-                ViewBag.Idattachment = identificationResult?.UserData.IdAttachment;
-                ViewBag.PhoneNumber = identificationResult?.UserData.Code;
-                ViewBag.EmailAddress = identificationResult?.UserData.Email;
+                ViewBag.FirstName = identificationResult?.UserData?.FirstName;
+                ViewBag.LastName = identificationResult?.UserData?.LastName;
+                ViewBag.MiddleName = identificationResult?.UserData?.MiddleName;
+                ViewBag.Personalattachment = identificationResult?.UserData?.PersonalAttachment;
+                ViewBag.Idnumber = identificationResult?.UserData?.IdNumber;
+                ViewBag.Idtype = identificationResult?.UserData?.IdType;
+                ViewBag.Dob = identificationResult?.UserData?.Dob;
+                ViewBag.Idattachment = identificationResult?.UserData?.IdAttachment;
+                ViewBag.PhoneNumber = identificationResult?.UserData?.Code;
+                ViewBag.EmailAddress = identificationResult?.UserData?.Email;
             }
-            //var bus = "wbus";
-            //var hotel = "whotel";
-            //var spa = "wspa";
-            //var events = "wevent";
-            //var cinema = "wmovie";00
 
 
-            //HttpResponseMessage busResponse = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={bus}");
+            var busResponse = await PicturesResonseAsync("wbus");
+            var cinemaResponse = await PicturesResonseAsync("wmovie");
+            var spaResponse = await PicturesResonseAsync("wspa");
+            var eventResposne = await PicturesResonseAsync("wevent");
+            var hotelResponse = await PicturesResonseAsync("whotel");
 
-            //if (busResponse.IsSuccessStatusCode)
-            //{
-            //    string busData = await busResponse.Content.ReadAsStringAsync();
-            //    var busImages = JsonConvert.DeserializeObject<List<string>>(busData);
-            //    Picures.Bus = busImages ?? new List<string>();
-            //}
-            //HttpResponseMessage cinemaResponse = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={cinema}");
-
-            //if (cinemaResponse.IsSuccessStatusCode)
-            //{
-            //    string cinemaData = await cinemaResponse.Content.ReadAsStringAsync();
-            //    var cinemaImages = JsonConvert.DeserializeObject<List<string>>(cinemaData);
-            //    Picures.Cinema = cinemaImages ?? new List<string>();
-            //}
-            //HttpResponseMessage spaResponse = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={spa}");
-
-            //if (spaResponse.IsSuccessStatusCode)
-            //{
-            //    string spaData = await spaResponse.Content.ReadAsStringAsync();
-            //    var spaImages = JsonConvert.DeserializeObject<List<string>>(spaData);
-            //    Picures.Spa = spaImages ?? new List<string>();
-            //}
-            //HttpResponseMessage eventResposne = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={events}");
-
-            //if (eventResposne.IsSuccessStatusCode)
-            //{
-            //    string eventData = await eventResposne.Content.ReadAsStringAsync();
-            //    var eventImages = JsonConvert.DeserializeObject<List<string>>(eventData);
-            //    Picures.Event = eventImages ?? new List<string>();
-            //}
-            //HttpResponseMessage hotelResponse = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={hotel}");
-
-            //if (hotelResponse.IsSuccessStatusCode)
-            //{
-            //    string hotelData = await hotelResponse.Content.ReadAsStringAsync();
-            //    var hotelImages = JsonConvert.DeserializeObject<List<string>>(hotelData);
-            //    Picures.Hotel = hotelImages ?? new List<string>();
-            //}
+            Picures.Bus = busResponse ?? new List<string>();
+            Picures.Cinema = cinemaResponse ?? new List<string>();
+            Picures.Event = eventResposne ?? new List<string>();
+            Picures.Hotel = hotelResponse ?? new List<string>();
+            Picures.Spa = spaResponse ?? new List<string>();
+            
             return View(Picures);
         }
 
@@ -103,6 +70,19 @@ namespace HulubejeBooking.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public async Task<List<string>> PicturesResonseAsync(string type) {
+            var _client = _httpClientFactory.CreateClient("CnetHulubeje");
+
+            HttpResponseMessage response = await _client.GetAsync(_client.BaseAddress + $"/Ecommerce/GetBannerImages?directory={type}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+                var images = JsonConvert.DeserializeObject<List<string>>(data);
+                return images ?? new List<string>();
+            }
+            return new List<string>();
         }
     }
 }
