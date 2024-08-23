1dce6a2647cb5fadf7686d84b3cb1374a4703b6b
Markos Beyene <markosbeyene1@gmail.com>
2024-08-23 15:34:11 +0300

    added reserve table controller and view and  get company settings model

---
+++ Controllers/ReserveTableController.cs
@@ -0,0 +1,70 @@
+﻿using CNET_ERP_V7_VoucherPrintDialogue.Models;
+using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models;
+using HulubejeBooking.Models.Temp;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace HulubejeBooking.Controllers
+{
+    public class ReserveTableController : Controller
+    {
+        private readonly AuthenticationManager _authenticationManager;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public ReserveTableController(AuthenticationManager authenticationManager, IHttpClientFactory httpClientFactory)
+        {
+            _authenticationManager = authenticationManager;
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var identificationResult = await _authenticationManager.identificationValid();
+
+            string? token = identificationResult?.UserData?.Token;
+            var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
+            _v7Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var getCompany = new HulubejeResponse<GetCompany>();
+            HttpResponseMessage getCompanyResponse = await _v7Client.GetAsync($"tablereservation/get?company={23}&branch={27}");
+            if (getCompanyResponse.IsSuccessStatusCode)
+            {
+                string responseData = await getCompanyResponse.Content.ReadAsStringAsync();
+                getCompany = responseData != null ? JsonConvert.DeserializeObject<HulubejeResponse<GetCompany>>(responseData) : new HulubejeResponse<GetCompany>();
+            }
+            var companysetting = new HulubejeResponse<GetCompanySetting>();
+            HttpResponseMessage companysettingResponse = await _v7Client.GetAsync($"routing/getcompanysetting?companyCode={23}&industryType=-10");
+            if (companysettingResponse.IsSuccessStatusCode)
+            {
+                string responseData = await companysettingResponse.Content.ReadAsStringAsync();
+                companysetting = responseData != null ? JsonConvert.DeserializeObject<HulubejeResponse<GetCompanySetting>>(responseData) : new HulubejeResponse<GetCompanySetting>();
+            }
+            var reserveTable = new ReserveTable
+            {
+                GetCompany = getCompany,
+                GetCompanySetting = companysetting
+            };
+            return View(reserveTable);
+        }
+        [HttpPost]
+        public IActionResult Index(ReservationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("ReservationConfirmation");
+            }
+
+            // Inspecting the validation errors
+            foreach (var state in ModelState)
+            {
+                foreach (var error in state.Value.Errors)
+                {
+                    Console.WriteLine($"Key: {state.Key}, Error: {error.ErrorMessage}");
+                }
+            }
+
+            ViewBag.Errors = ModelState.Values.SelectMany(v => v.Errors).Select(e => e.ErrorMessage).ToList();
+
+            return View();
+        }
+    }
+}
---
+++ Models/Temp/GetCompanySetting.cs
@@ -0,0 +1,56 @@
+﻿namespace HulubejeBooking.Models.Temp
+{
+    public class GetCompanySetting
+    {
+        public bool RecieveOrderOnClosingHours { get; set; }
+        public int? MinimumStock { get; set; }
+        public int? ControlStock { get; set; }
+        public DateTime? CheckinTime { get; set; }
+        public DateTime? CheckoutTime { get; set; }
+        public string? CancilationPolicy { get; set; }
+        public bool? IsEnabledDelivery { get; set; }
+        public bool? IsEnabledPickupAtBranch { get; set; }
+        public bool IsTaxInclusive { get; set; }
+        public DateTime ServerTime { get; set; }
+    }
+
+    public class GetCompany
+    {
+        public List<object>? Tables { get; set; }
+        public List<Occasion>? Occasions { get; set; }
+        public int ReservationFee { get; set; } 
+        public DateTime ServerTime { get; set; }
+    }
+
+    public class Occasion
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class ReserveTable
+    {
+        public HulubejeResponse<GetCompany>? GetCompany { get; set; }
+        public HulubejeResponse<GetCompanySetting>? GetCompanySetting { get; set; }
+    }
+
+    public class ReservationModel
+    {
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public int NumberOfGuests { get; set; }
+        public int Duration { get; set; }
+        public int OccasionId { get; set; }
+        public string? SpecialRequirements { get; set; }
+        public decimal ReservationFee { get; set; }
+
+        // Constructor
+        public ReservationModel()
+        {
+            Date = DateTime.Now.Date;
+            Time = DateTime.Now;
+            NumberOfGuests = 1;
+            SpecialRequirements = string.Empty;
+        }
+    }
+}
