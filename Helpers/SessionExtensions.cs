917431ef63d250ec99662776b032e12eb0c3185c
Markos Beyene <markosbeyene1@gmail.com>
2024-10-24 16:02:47 +0300

    added a helpers folder and sessionextentions class for managing sessions

---
+++ Helpers/SessionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace HulubejeBooking.Helpers
+{
+    public static class SessionExtensions
+    {
+        // Set object as JSON in session
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonConvert.SerializeObject(value));
+        }
+
+        // Get object from session by deserializing JSON
+        public static T? GetObjectFromJson<T>(this ISession session, string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
