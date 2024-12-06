78b3adb531347927e7b3a57be11737657dc676d1
Markos Beyene <markosbeyene1@gmail.com>
2024-12-06 10:20:46 +0300

    extraceted cart items from session memory and passed them to the view and made an api request to fetch available schedules fot the selected date in schedules controller

--- Controllers/SpaController/SchedulesController.cs
+++ Controllers/SpaController/SchedulesController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using HulubejeBooking.Helpers;
+using HulubejeBooking.Models.HotelModels;
 
 namespace HulubejeBooking.Controllers.SpaController
 {
@@ -19,7 +20,7 @@ namespace HulubejeBooking.Controllers.SpaController
             _authenticationManager = authenticationManager;
             _httpClientFactory = httpClientFactory;
         }
-        public async Task<IActionResult> IndexAsync()
+        public async Task<IActionResult> Index(int company, string department)
         {
             var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
             var token = "";
@@ -40,22 +41,46 @@ namespace HulubejeBooking.Controllers.SpaController
                 ViewBag.PhoneNumber = identificationResult?.UserData.Code;
                 ViewBag.EmailAddress = identificationResult?.UserData.Email;
             }
+            if (HttpContext.Session.TryGetValue("CartItems", out var getspaReservationBytes))
+            {
+                List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>("CartItems") ?? new List<CartItem>();
+                //GetCompanySchedule companyschedule = HttpContext.Session.GetObjectFromJson<GetCompanySchedule>("companyschedule") ?? new GetCompanySchedule();
 
-            List<CartItem> cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+                _v7Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                var getspareservation = await GetspareservationResponseAsync(company.ToString(), department, "50", "2024-11-22T08:00:00.000", "2024-11-22T17:00:00.000",
+                    token) ?? new HulubejeResponse<List<Schedules>>();
+                var companyDetail = new CompanyDetailModel
+                {
+                    CompanyCode = company
+                };
+                var spareservation = JsonConvert.SerializeObject(getspareservation);
+                //HttpContext.Session.SetString("GetSpaReservation", spareservation);
+                var ScheduleView = new ScheduleView { CartItem = cart, Schedules = getspareservation, CompanyDetailModel = companyDetail };
+                return View(ScheduleView);
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Session Has Expired Please Restart the Booking Process";
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+        public async Task<HulubejeResponse<List<Schedules>>> GetspareservationResponseAsync(string company, string department, string duration, 
+            string startTime, string endtime, string? token)
+        {
+            var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
 
             _v7Client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             var getspareservation = new HulubejeResponse<List<Schedules>>();
-            HttpResponseMessage getspareservationResponse = await _v7Client.GetAsync($"spareservation/available?company=122225&department=6&duration=50&" +
-                $"CompanyStartTime=2024-11-22T08:00:00.000&CompanyEndTime=2024-11-22T17:00:00.000");
+            HttpResponseMessage getspareservationResponse = await _v7Client.GetAsync($"spareservation/available?company={company}" +
+                $"&department={department}&duration={duration}&" +
+                $"CompanyStartTime={startTime}&CompanyEndTime={endtime}");
             if (getspareservationResponse.IsSuccessStatusCode)
             {
                 string getcompanyscheduleData = await getspareservationResponse.Content.ReadAsStringAsync();
                 getspareservation = JsonConvert.DeserializeObject<HulubejeResponse<List<Schedules>>>(getcompanyscheduleData);
             }
-            var spareservation = JsonConvert.SerializeObject(getspareservation);
-            //HttpContext.Session.SetString("GetSpaReservation", spareservation);
-            var ScheduleView = new ScheduleView { CartItem = cart , Schedules = getspareservation};
-            return View(ScheduleView);
+            return getspareservation ?? new HulubejeResponse<List<Schedules>>();
         }
     }
 
