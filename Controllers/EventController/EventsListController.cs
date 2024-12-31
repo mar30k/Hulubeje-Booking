1376ab5a504aff6edb64b2a60c916c344d180276
Markos Beyene <markosbeyene1@gmail.com>
2024-12-31 15:00:24 +0300

    Add EventsListController and update event listing view
    The `EventsListController.cs` file has been significantly updated:
    - Added new using directives for various namespaces.
    - Introduced `HulubejeBooking.Controllers.EventController` namespace.
    - Added `EventsListController` class with private readonly fields.
    - Implemented constructor to initialize fields.
    - Added `Index` action method for event listing and session management.
    - Added `SaveCart` action method to handle cart operations.
    
    The `Index.cshtml` file has been updated:
    - Included new using directives and set model to `EventResponse`.
    - Added variables `isValid` and `isLoggedIn` from `ViewBag`.
    - Enhanced styles for cards and buttons.
    - Added script to store session values.
    - Updated main content to display event details and handle cart operations.
    - Added JavaScript functions for cart management and AJAX requests.

---
+++ Controllers/EventController/EventsListController.cs
@@ -0,0 +1,75 @@
+﻿using HulubejeBooking.Controllers.Authentication;
+using HulubejeBooking.Models.SpaModels;
+using HulubejeBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+using HulubejeBooking.Models.EventModels;
+using HulubejeBooking.Models.HotelModels;
+using HulubejeBooking.Helpers;
+
+namespace HulubejeBooking.Controllers.EventController
+{
+    public class EventsListController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private readonly IHttpContextAccessor? _httpContextAccessor;
+        private readonly AuthenticationManager _authenticationManager;
+
+        public EventsListController(IHttpClientFactory httpClientFactory, IHttpContextAccessor httpContextAccessor, AuthenticationManager authenticationManager )
+        {
+            _authenticationManager = authenticationManager;
+            _httpClientFactory = httpClientFactory;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<IActionResult> Index(int code, int articleCode, string name, int organizer)
+        {
+            CompanyDetailModel companyDetail = new() { Code = code, Organizer = organizer, ArticleCode = articleCode, Name = name };
+
+            var _v7Client = _httpClientFactory.CreateClient("HulubejeBooking");
+            var token = "";
+            var identificationResult = await _authenticationManager.identificationValid();
+            if (identificationResult != null)
+            {
+                token = identificationResult.UserData.Token;
+                ViewBag.isVaild = identificationResult.isValid;
+                ViewBag.isLoggedIn = identificationResult.isLoggedIn;
+                ViewBag.FirstName = identificationResult?.UserData.FirstName;
+                ViewBag.LastName = identificationResult?.UserData.LastName;
+                ViewBag.MiddleName = identificationResult?.UserData.MiddleName;
+                ViewBag.Personalattachment = identificationResult?.UserData.PersonalAttachment;
+                ViewBag.Idnumber = identificationResult?.UserData.IdNumber;
+                ViewBag.Idtype = identificationResult?.UserData.IdType;
+                ViewBag.Dob = identificationResult?.UserData.Dob;
+                ViewBag.Idattachment = identificationResult?.UserData.IdAttachment;
+                ViewBag.PhoneNumber = identificationResult?.UserData.Code;
+                ViewBag.EmailAddress = identificationResult?.UserData.Email;
+            }
+            var selectedEvent = new EventResponse();
+            if (HttpContext.Session.TryGetValue("EventResponse", out var eventResponseBytes))
+            {
+
+                var eventResponseString = Encoding.UTF8.GetString(eventResponseBytes); 
+                var eventResponse = eventResponseString != null
+                ? JsonConvert.DeserializeObject<HulubejeResponse<List<EventResponse>>>(eventResponseString) ?? new HulubejeResponse<List<EventResponse>>()
+                : new HulubejeResponse<List<EventResponse>>();
+                selectedEvent = eventResponse?.Data?
+                    .Where(c => c.Code == companyDetail?.Code && c.Organizer.ToString() == companyDetail?.Organizer?.ToString()).FirstOrDefault();
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Session Has Expired Please Restart the Booking Process";
+                return RedirectToAction("Index", "Home");
+            }   
+            return View(selectedEvent);
+        }
+
+        [HttpPost]
+        public IActionResult SaveCart([FromBody] List<CartItem> cartItems)
+        {
+            // Save the cart items in the session
+            HttpContext.Session.SetObjectAsJson("EventCartItems", cartItems);
+            return Ok();
+        }
+    }
+}
